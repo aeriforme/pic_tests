880a157d3c62b258285931bb3a397115bf77bbef
kevinambro <117300522+kevinambro@users.noreply.github.com>
2022-11-30 16:47:27 +0100

    Delete run.sh

--- 2d_laser_plasma/run.sh
+++
@@ -1,9 +0,0 @@
-cd /Users/kevinambrogioni/Thesis/Examples/pic_tests/2d_laser_plasma/smilei
-export OMP_NUM_THREADS=1 
-mpirun -np 2 ./smilei input.py 
-
-cd /Users/kevinambrogioni/Thesis/Examples/pic_tests/2d_laser_plasma/warpx
-mpirun -np 2 ./warpx.2d.MPI.OMP.DP.PDP.OPMD.QED input.txt
-
-cd /Users/kevinambrogioni/Thesis/Examples/pic_tests/2d_laser_plasma/epoch
-echo Data | mpirun -np 2 ./epoch2d 
